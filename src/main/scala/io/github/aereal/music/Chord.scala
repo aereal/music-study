41ee139353acd8594595d1d2755330c352589c72
aereal <aereal@aereal.org>
2017-09-13 18:41:16 +0900

    Make Chord as abstract

--- src/main/scala/io/github/aereal/music/Chord.scala
+++ src/main/scala/io/github/aereal/music/Chord.scala
@@ -5,12 +5,14 @@ package music
 
 import scala.collection.SortedSet
 
-case class Chord private (tones: SortedSet[pitch.Pitch]) {
+abstract class Chord(tones: SortedSet[pitch.Pitch]) {
   def root: pitch.Pitch = tones.head
 }
 object Chord {
+  private case class Impl(tones: SortedSet[pitch.Pitch]) extends Chord(tones)
+
   def apply(p1: pitch.Pitch, p2: pitch.Pitch, rest: pitch.Pitch*): Chord = {
     val tones = p1 :: p2 :: rest.toList
-    Chord(SortedSet(tones: _*))
+    Impl(SortedSet(tones: _*))
   }
 }
