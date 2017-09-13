fa013286db424818fad5c64a476f6ab39ffba8b3
aereal <aereal@aereal.org>
2017-09-13 18:42:34 +0900

    import

--- src/main/scala/io/github/aereal/music/Chord.scala
+++ src/main/scala/io/github/aereal/music/Chord.scala
@@ -3,15 +3,16 @@ package github
 package aereal
 package music
 
+import pitch.Pitch
 import scala.collection.SortedSet
 
-abstract class Chord(tones: SortedSet[pitch.Pitch]) {
-  def root: pitch.Pitch = tones.head
+abstract class Chord(tones: SortedSet[Pitch]) {
+  def root: Pitch = tones.head
 }
 object Chord {
-  private case class Impl(tones: SortedSet[pitch.Pitch]) extends Chord(tones)
+  private case class Impl(tones: SortedSet[Pitch]) extends Chord(tones)
 
-  def apply(p1: pitch.Pitch, p2: pitch.Pitch, rest: pitch.Pitch*): Chord = {
+  def apply(p1: Pitch, p2: Pitch, rest: Pitch*): Chord = {
     val tones = p1 :: p2 :: rest.toList
     Impl(SortedSet(tones: _*))
   }
